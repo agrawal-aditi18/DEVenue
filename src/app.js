ed7452a2f7074f432b2cd33765ab40dc9aa3451b
aditi agrawal <aditiagrawalsofficial@gmail.com>
2025-08-24 09:14:39 -0600

    explored RestAPIs

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res)=>{
+    res.send({firstName: "Aditi", lastName: "Agrawal"});
+})
+
+app.post("/user", (req,res) => {
+    //saving data to DB
+    res.send("Data successfully saved to the database!")
+});
+
+app.delete("/user", (req,res)=>{
+    res.send("Deleted successfully!");
+});
+
 app.use("/test", (req, res)=>{
     res.send("Hello from test route!");
-})
-app.use("/hello", (req, res)=>{
-    res.send("Hello hello hello!");
-})
+});
 
 app.listen(7777, ()=> {
     console.log("Server is successfully listening on port 7777...");
