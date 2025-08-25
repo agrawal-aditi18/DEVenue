5f3e5c129da3ae75cfa8a4ab8bd54c9bd5ba5786
aditi agrawal <aditiagrawalsofficial@gmail.com>
2025-08-25 11:49:07 -0600

    create get-APIs

--- src/app.js
+++ src/app.js
@@ -3,20 +3,44 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
 
-app.post("/signup", async (req, res) =>{
-  //Creating a new instance of the User Model
-  const user = new User ( {
-    firstName : "Ramya",
-    lastName : "Sharma",
-    emailId: "ram@sh.com",
-    password: "krish31"
-  });
+app.use(express.json()); //now this middleware is activated for all the routes to convert the json data into js obj
 
+app.post("/signup", async (req, res) =>{
+  // console.log(req.body); //becz we are requesting data from body section
+  //Creating a new instance of the User Model
+  const user = new User(req.body); //creating a new user which i have got from the request
   try {
     await user.save(); //data saved to DB
     res.send("User added successfully!");
   } catch (error) {
     res.status(400).send("Erro saving the user:" + err.message);
+  }
+});
+
+app.get("/user", async(req , res) => {
+  const userEmail = req.body.emailId;
+  try {
+     const users = await User.find({emailId : userEmail});
+    if(users.length === 0){
+      res.status(404).send("User not found");
+    }
+    else{
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+
+});
+
+//Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try{
+    const users = await User.find({});
+    res.send(users);
+  }
+  catch(err){
+    res.status(400).send("Something went wrong");
   }
 })
 
