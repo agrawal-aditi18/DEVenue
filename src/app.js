44c00767b3d985fb60a5234f2d1e6c4e4ed69461
aditi agrawal <aditiagrawalsofficial@gmail.com>
2025-08-24 23:32:19 -0600

    playing with route handlers

--- src/app.js
+++ src/app.js
@@ -2,24 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name", (req, res)=>{
-    // console.log(req.query);
-    console.log(req.params);
-    res.send({firstName: "Aditi", lastName: "Agrawal"});
+app.use("/", 
+[(req, res, next)=>{
+    // res.send("Handling the route user 1!");
+    next();
+},
+(req, res, next)=>{
+    // res.send("Handling the route user 2!");
+    next();
+},
+],
+(req, res, next)=>{
+    // res.send("Handling the route user 3!");
+    next();
+},
+(req, res, next)=>{
+    // res.send("Handling the route user 4!");
+    next();
+},
+);
+
+app.get("/user", (req ,res, next)=>{
+res.send("Handle/ route");
 })
-
-app.post("/user", (req,res) => {
-    //saving data to DB
-    res.send("Data successfully saved to the database!")
-});
-
-app.delete("/user", (req,res)=>{
-    res.send("Deleted successfully!");
-});
-
-app.use("/test", (req, res)=>{
-    res.send("Hello from test route!");
-});
 
 app.listen(7777, ()=> {
     console.log("Server is successfully listening on port 7777...");
