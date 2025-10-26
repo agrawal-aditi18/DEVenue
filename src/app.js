18fa9a8b0acfc035a06029c7f2c5641f3bfd5dfb
aditi agrawal <aditiagrawalsofficial@gmail.com>
2025-10-26 13:53:25 -0600

    feat: chat

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database")
 const app = express();
 const cookieParser = require("cookie-parser")
 const cors = require("cors");
+const http = require("http");
 
 
 app.use(
@@ -25,16 +26,23 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/" , authRouter);
 app.use("/" , profileRouter);
 app.use("/" ,  requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
+
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(7777, () => {
+    server.listen(7777, () => {
       console.log("Server is successfully listening on port 7777...");
     });
   })
